fe1514ec36031290c5aafbf912383b219f581619
Umisia <maciaszek89@gmail.com>
2019-10-29 19:24:22 +0000

    html and css

---
+++ app.js
@@ -0,0 +1,68 @@
+/*
+TODO:
+    Limit number input
+    Disallow . from being entered multiple times
+    Clean up structure
+*/
+
+
+  // Shortcut to get elements
+
+  // Variables
+  // Calculator screen where result is displayed
+    // Equal button
+    // List of numbers
+     // List of operators
+   // Current number
+    // First number
+    // Result
+  
+
+  // When: Number is clicked. Get the current number selected
+  // Otherwise, add digit to previous number (this is a string!)
+    // Display current number
+
+
+
+  // When: Operator is clicked. Pass number to oldNum and save operator
+  // Reset result in attr
+ 
+
+  // When: Equals is clicked. Calculate result
+  
+
+    // Convert string input to numbers
+ 
+
+    // Perform operation (switch)
+  
+        // If equal is pressed without an operator, keep number and continue
+   
+
+    // If NaN or Infinity returned
+     // If result is not a number; set off by, eg, double-clicking operators
+        // If result is infinity, set off by dividing by zero
+         // Break calculator
+        // And show reset button
+    
+
+    // Display result, finally!
+   
+    // Now reset oldNum & keep result
+    
+
+  // When: Clear button is pressed. Clear everything
+  
+
+  /* The click events */
+
+  // Add click event to numbers
+  
+  // Add click event to operators
+  
+  // Add click event to equal sign
+  
+  // Add click event to clear button
+ 
+  // Add click event to reset button
+ 
