1d838d6e11e31097f4a3c33f2cef6d99e1e7fa4e
Umisia <maciaszek89@gmail.com>
2019-11-13 19:43:56 +0000

    finished

--- app.js
+++ app.js
@@ -1,80 +1,114 @@
-const nums = document.querySelectorAll(".nums");//list
-let viewer = document.querySelector("#viewer");
+const nums = document.querySelectorAll(".nums");
+const viewer = document.querySelector("#viewer");
 const ops = document.querySelectorAll(".ops");
-let equal = document.querySelector("#equal");
+const equal = document.querySelector("#equal");
 const clear = document.querySelector("#clear");
+const liveViewer = document.querySelector(".live-view");
+const sheet = window.document.styleSheets[0];
+const checkbox = document.querySelector('input[type="checkbox"]');
+
+
 
 let theNumber = "",
-    oldNumber = "",
     op = "",
     resultNumber = "";
 
-for (let i=0;i<nums.length; i++){
-    nums[i].addEventListener("click", function() {
-        if (resultNumber){
-            theNumber = this.getAttribute("data-num");
-            resultNumber = "";   
-        } else {
-            theNumber += this.getAttribute("data-num");
-            if (theNumber.length > 8){
-                theNumber = theNumber.slice(0,9);
+function getNumbers() {  
+        if (op=== "=") {
+            resultNumber= "";
+            op = "";
+            theNumber= "";
+        } 
+        theNumber += this.getAttribute("data-num");  
+        liveViewer.innerHTML += this.getAttribute("data-num");  
+        viewer.innerHTML = theNumber;                    
+    }
+
+
+function getOp() {        
+        if (op === "="){
+            liveViewer.innerHTML = resultNumber;       
+            op= this.getAttribute("data-ops");
+            theNumber= ""; 
+            liveViewer.innerHTML += op;  
+        } else if (theNumber!== ""){ 
+            evaluateSwitch(op);             
+            op = this.getAttribute("data-ops");  
+            liveViewer.innerHTML += op;         
+            theNumber = ""; 
             }
-        }
-        viewer.innerHTML = theNumber;   
-    });
-}
- 
-for (let i=0;i<ops.length; i++){
-    ops[i].addEventListener("click", function(){
-        if (theNumber !== ""){
-        oldNumber = theNumber;
-        theNumber = "";
-        op = this.getAttribute("data-ops");
-        }
-        
-    });
+        } 
+
+
+function evaluate(op) {
+    theNumber = parseFloat(theNumber);  
+    let equation = liveViewer.textContent;
+    resultNumber = eval(equation);
+    viewer.innerHTML = resultNumber;
 }
 
-equal.addEventListener("click", function() {
-    oldNumber = parseFloat(oldNumber);
-    theNumber = parseFloat(theNumber);
-    console.log("oldNumber:", oldNumber, ", op:", op, ", theNumber:", theNumber);
+function evaluateSwitch(op) {
+    theNumber = parseFloat(theNumber);      
     switch (op) {
-        case "+":
-            resultNumber = oldNumber + theNumber;
+        case "+":           
+            resultNumber = resultNumber + theNumber;
             break;
         case "-":
-            resultNumber = oldNumber - theNumber;
+            resultNumber = resultNumber - theNumber;
             break;
         case "*":
-            resultNumber = oldNumber * theNumber;
+            resultNumber = resultNumber * theNumber;
             break;
         case "/":
-            resultNumber = oldNumber / theNumber;
+            resultNumber = resultNumber / theNumber;
             break;
         default:
             resultNumber = theNumber;
             break;
-    }
-    if ( (!isFinite(resultNumber)) || (isNaN(resultNumber)) ){
-            clearAll("Error");
-            
-    } else {
-            viewer.innerHTML = resultNumber;
-            oldNumber = 0;
-            theNumber = resultNumber; 
-        }            
+    };
+    viewer.innerHTML = resultNumber;
+}
+
+for (let i=0;i<nums.length; i++){
+        nums[i].addEventListener("click", getNumbers);
+}
+
+for (let i=0;i<ops.length; i++){
+    ops[i].addEventListener("click", getOp);
+}
+        
+liveViewer.addEventListener("animationend", function (){
+    liveViewer.innerHTML = "";
+    liveViewer.classList.remove("activated-vert");
 });
 
-let clearAll = function (message) {
+equal.addEventListener("click", function() {  
+    liveViewer.classList.add("activated-vert");   
+    evaluate(op);
+    if ( (!isFinite(resultNumber)) || (isNaN(resultNumber)) ){
+        clearAll("Error");            
+    }  
+    op = this.getAttribute("data-ops");   
+});
+
+function clearAll(message) {
     oldNumber = "";
     theNumber = "";
     resultNumber = "";
+    op = "";
+    liveViewer.innerHTML = "";
     viewer.innerHTML = message;
 }
 
-
 clear.addEventListener("click", function() {
-    clearAll("0");
+    clearAll("");
 });
- 
+
+
+checkbox.addEventListener('change', function () {
+    if (checkbox.checked) {  
+        sheet.insertRule('button:hover { background-color: #ee496a;}', sheet.cssRules.length);
+    } else {
+        sheet.insertRule('button:hover { background-color: greenyellow;}', sheet.cssRules.length);
+    }
+});
